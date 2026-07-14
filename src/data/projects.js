39ae63594fa434b7839c2fff074198263c5d7b9a
Almanex <25957600+Almanex@users.noreply.github.com>
2026-07-14 20:42:21 +0200

    Публикация

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
-import classic_windows_image_viewer from './projects/classic-windows-image-viewer.json';
 import flowguide from './projects/flowguide.json';
 import gemma_local_desktop from './projects/gemma-local-desktop.json';
 import lingospaced from './projects/lingospaced.json';
+import classic_windows_image_viewer from './projects/classic-windows-image-viewer.json';
 import mushroom_spot from './projects/mushroom-spot.json';
 import portable_app_sync from './projects/portable-app-sync.json';
 import rokey from './projects/rokey.json';
@@ -12,10 +12,10 @@ import shortcutdock from './projects/shortcutdock.json';
 import webpconverter from './projects/webpconverter.json';
 
 export const PROJECTS_DATA = {
-  "classic-windows-image-viewer": classic_windows_image_viewer,
   "flowguide": flowguide,
   "gemma-local-desktop": gemma_local_desktop,
   "lingospaced": lingospaced,
+  "classic-windows-image-viewer": classic_windows_image_viewer,
   "mushroom-spot": mushroom_spot,
   "portable-app-sync": portable_app_sync,
   "rokey": rokey,
